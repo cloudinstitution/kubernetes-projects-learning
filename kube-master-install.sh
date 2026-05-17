bd6fe6ddac7801ab92548c049ae6298577dad156
cloudinstitution <134688786+cloudinstitution@users.noreply.github.com>
2026-05-17 10:56:24 +0530

    Update kube-master-install.sh

--- kube-master-install.sh
+++ kube-master-install.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+# Disable swap
 swapoff -a
+sed -i '/swap/d' /etc/fstab
 
+# Install required package
 dnf install -y iproute-tc
 
+# Kernel modules
 modprobe overlay
 modprobe br_netfilter
 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+# Sysctl settings
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
+
 sysctl --system
+
+# Disable SELinux
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Kubernetes repo
 KUBERNETES_VERSION=v1.29
-PROJECT_PATH=prerelease:/main
+
 cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -30,6 +40,9 @@ enabled=1
 gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
 EOF
+
+# CRI-O repo
+PROJECT_PATH=prerelease:/main
 
 cat <<EOF | tee /etc/yum.repos.d/cri-o.repo
 [cri-o]
@@ -40,16 +53,27 @@ gpgcheck=1
 gpgkey=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/repodata/repomd.xml.key
 EOF
 
+# Install packages
 dnf install -y cri-o kubelet kubeadm kubectl
+
+# Enable services
 systemctl enable --now crio
 systemctl enable --now kubelet
 
+# Initialize Kubernetes cluster
 kubeadm init --pod-network-cidr=10.244.0.0/16
+
+# Configure kubectl
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Export kubeconfig
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Install Flannel network plugin
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-
-
+# Verify cluster
+kubectl get nodes
+kubectl get pods -A
